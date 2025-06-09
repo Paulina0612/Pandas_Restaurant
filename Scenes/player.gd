bd97a2538f641e41c9bc434b4ac5de04c64894a4
Paulina Bartczak <paulinabartczak2@onet.pl>
2025-06-09 18:21:39 +0200

    Standing animations added.

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -3,11 +3,14 @@ extends CharacterBody2D
 @export var speed = 400 # How fast the player will move (pixels/sec).
 var screen_size # Size of the game window.
 var if_cutting = false
-var if_carrying = false
+var if_carrying = true
+var dir = Vector2.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
+	$AnimatedSprite2D.animation = "standing_down"
+	dir = Vector2.DOWN
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -18,12 +21,16 @@ func _process(_delta):
 		var panda_velocity = Vector2.ZERO # The player's movement vector.
 		if Input.is_action_pressed("ui_right"):
 			panda_velocity.x += 1
+			dir = Vector2.RIGHT
 		if Input.is_action_pressed("ui_left"):
 			panda_velocity.x -= 1
+			dir = Vector2.LEFT
 		if Input.is_action_pressed("ui_down"):
 			panda_velocity.y += 1
+			dir = Vector2.DOWN
 		if Input.is_action_pressed("ui_up"):
 			panda_velocity.y -= 1
+			dir = Vector2.UP
 
 		if panda_velocity.length() > 0:
 			panda_velocity = panda_velocity.normalized() * speed
@@ -54,6 +61,27 @@ func _process(_delta):
 				$AnimatedSprite2D.animation = "walk_up_carrying"
 			elif panda_velocity.y > 0:
 				$AnimatedSprite2D.animation = "walk_down_carrying"
+		
+		if velocity == Vector2.ZERO:
+			if if_carrying:
+				if dir == Vector2.DOWN:
+					$AnimatedSprite2D.animation = "standing_down_carrying"
+				elif dir == Vector2.UP:
+					$AnimatedSprite2D.animation = "standing_up_carrying"
+				elif dir == Vector2.RIGHT:
+					$AnimatedSprite2D.animation = "standing_right_carrying"
+				elif dir == Vector2.LEFT:
+					$AnimatedSprite2D.animation = "standing_left_carrying"
+			else:
+				if dir == Vector2.DOWN:
+					$AnimatedSprite2D.animation = "standing_down"
+				elif dir == Vector2.UP:
+					$AnimatedSprite2D.animation = "standing_up"
+				elif dir == Vector2.RIGHT:
+					$AnimatedSprite2D.animation = "standing_right"
+				elif dir == Vector2.LEFT:
+					$AnimatedSprite2D.animation = "standing_left"
+					
 	else: 
 		$AnimatedSprite2D.animation = "cutting"
 
