8fc596eff2f893499b904b28075cf83d8d4e0085
Paulina Bartczak <paulinabartczak2@onet.pl>
2025-06-14 20:49:40 +0200

    Small fixes.

--- Scenes/level.gd
+++ Scenes/level.gd
@@ -2,15 +2,6 @@ extends Node2D
 class_name Level
 
 static var label_text = ""
-
-
-func _on_fail_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Fail.tscn")
-
-
-func _on_success_button_2_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Success.tscn")
-	
 	
 func _process(_delta: float) -> void:
 	$Label.text = label_text
